b70af453efbba1b39402ba0cfc5a88def10cce17
Homesweethome <Homesweethome@users.noreply.github.com>
2018-08-01 09:51:03 +0700

    [+] Необработанная ошибка

--- EpReader/App.xaml.cs
+++ EpReader/App.xaml.cs
@@ -12,6 +12,16 @@ namespace EpReader
     /// </summary>
     public partial class App : Application
     {
-        
+        public App() : base()
+        {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string errorMessage = string.Format("Необработанная ошибка: {0}", e.Exception.Message);
+            MessageBox.Show(errorMessage, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
